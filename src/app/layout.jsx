473e7657ed1aa39586ce84700acd6e141701e347
NeerajQA <neeraj1264qa@gmail.com>
2025-06-03 16:06:52 +0530

    change meta data and logoicon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,15 +4,15 @@ import "./globals.css";
 import { Montserrat, Lora } from 'next/font/google'
 
 export const metadata = {
-  title: 'Treasured Care For You',
-  description: 'Providing nurturing support and care services',
+  title: 'Empowering Independence with Compassionate Support',
+  description: 'Discover a community where independence is cherished. Treasured Care For You provides dedicated support tailored to your needs under the NDIS.',
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en"  className="font-heading">
           <head>
-        <link rel="icon" href="/logo.jpg" sizes="128x128" type="image/png" />
+        <link rel="icon" href="/logo-bgremove.png" sizes="128x128" type="image/png" />
       </head>
       <body className="font-heading">
         <Navbar />
